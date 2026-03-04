b14734d6120a420ee886d2e6365615eca7ea93b1
ABD ELHAFID AZLIGUE <abdelhafidazligue162@gmail.com>
2026-03-04 14:19:25 +0000

    added checkBanned controller, and conception folder with use case and class diagrams

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'role' => \App\Http\Middleware\checkRole::class
+            'role' => \App\Http\Middleware\checkRole::class,
+            'banned' => \App\Http\Middleware\checkBanned::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
